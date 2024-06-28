66aa6df736cd23c750bee1cb2e7e8aab0514f86c
intellectus-rojiwon <jiwon.ro@int2.us>
2024-06-28 13:38:28 +0900

    환경변수 적용 테스트

--- src/express.ts
+++ src/express.ts
@@ -7,3 +7,7 @@ export const app = express();
 app.use(bodyParser.json(), cookieParser());
 
 app.get('/test', (_, res) =>  res.json(process.env))
+
+if(process.env['NODE_ENV'] !== 'production'){
+    throw Error('환경 변수 적용 테스트')
+}
