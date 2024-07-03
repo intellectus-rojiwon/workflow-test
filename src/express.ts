dc87d060f377be1bc2db740411b71f9dcf7680b9
intellectus-rojiwon <jiwon.ro@int2.us>
2024-07-03 14:11:41 +0900

    lambda version test

--- src/express.ts
+++ src/express.ts
@@ -1,13 +1,9 @@
-import * as bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import express from 'express';
+import * as bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
 
 export const app = express();
 
 app.use(bodyParser.json(), cookieParser());
 
-app.get('/test', (_, res) =>  res.json(process.env))
-
-if(process.env['NODE_ENV'] !== 'production'){
-    throw Error('환경 변수 적용 테스트')
-}
+app.get("/test", (_, res) => res.json(process.env));
--- src/lambda.ts
+++ src/lambda.ts
@@ -2,4 +2,4 @@ import sls from "@codegenie/serverless-express";
 import { Handler } from "aws-lambda";
 import { app } from "./express";
 
-export const handler: Handler = sls({ app })
+export const handler: Handler = sls({ app });
